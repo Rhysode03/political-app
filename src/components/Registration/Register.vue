777af2eff7e6f9ac65fb353310a6107cb637940d
Kyle Calder <kylebcalder03@gmail.com>
2020-12-17 21:43:33 -0700

    Changed login to register and added bootstrap

---
+++ src/components/Registration/Register.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="loginName">
+      <label for="name">Full name:</label>
+      <input type="text" id="name">
+    </div>
+    <div class="">
+      <input type="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    
+  },
+  computed: {
+
+  },
+};
+</script>
+
+<style>
+</style>
