23b3bd7b7140511b6e47878f19bf36475f9e8c86
Kyle Calder <kylebcalder03@gmail.com>
2020-12-29 20:47:02 -0700

    Added NewUser, set up bootstrap and vue router

--- src/components/Registration/Register.vue
+++ src/components/Registration/Register.vue
@@ -1,31 +1,45 @@
 <template>
   <div>
-    <div class="userName">
-      <label for="userName"> Username:</label>
-      <input type="text" id="username">
-    </div>
-    <div class="Password">
-      <label for="password"> Password:</label>
-      <input type="password" id="password">
-    </div>
-    <div class="name">
-      <label for="name">Enter Name:</label>
-      <input type="text" id="name">
-    </div>
-    <div class="">
-      <input type="">
+    <div class="input-group">
+      <div class="title">
+        <h1>Sign In</h1>
+        <router-link to="/NewUser" active-class="active" tag="li">
+          <h5>Or <a>create an account</a></h5>
+        </router-link>
+      </div>
+      <form>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+          />
+          <small id="emailHelp" class="form-text text-muted"
+            >We'll never share your email with anyone else.</small
+          >
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+          />
+        </div>
+        <button type="submit" class="btn btn-primary">Sign In</button>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  components: {
-    
-  },
-  computed: {
-
-  },
+  components: {},
+  computed: {},
 };
 </script>
 
