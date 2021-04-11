cbad7457ba84504bdc10922374ebdac1eee3d7bc
Kyle Calder <kylebcalder03@gmail.com>
2021-04-11 00:18:58 -0600

    added bootstrap

--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@ import App from './App.vue'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import VueRouter from 'vue-router'
-
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 Vue.use(VueRouter)
 
 
