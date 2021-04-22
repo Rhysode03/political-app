023d1b93a29e26ee27837a4c53db8f74443ef583
Kyle Calder <kylebcalder03@gmail.com>
2021-04-21 23:42:45 -0600

    Edited main.js

--- src/main.js
+++ src/main.js
@@ -2,12 +2,22 @@ import Vue from 'vue'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import VueRouter from 'vue-router'
+<<<<<<< HEAD
 import VueResource from 'vue-resource'
 
 import App from './App.vue'
 import { routes } from '.routes.js'
 import store from './store/store.js'
 
+=======
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+>>>>>>> master
 Vue.use(VueRouter)
 
 
